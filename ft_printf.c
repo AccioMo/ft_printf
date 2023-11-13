2e9f46b8368a995108a942937f472b4c1f1a09fe
Mohammed Zeggaf <mzeggaf@e3r3p9.1337.ma>
2023-11-13 18:30:05 +0100

    adding ft_printf

---
+++ ft_printf.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzeggaf <mzeggaf@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/12 22:23:59 by mzeggaf           #+#    #+#             */
+/*   Updated: 2023/11/13 15:13:05 by mzeggaf          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+char	*ft_strchr(const char *str, int c)
+{
+	while (*str && *str != (char)c)
+		str++;
+	if (!(*str) && (char)c != '\0')
+		return (NULL);
+	return ((char *)str);
+}
+
+int	ft_printf(const char *str, ...)
+{
+	int		len;
+	va_list	args;
+
+	len = 0;
+	va_start(args, str);
+	while (*str)
+	{
+		if (*str == '%')
+		{
+			str++;
+			len += ft_printf_multiverse(*str, args);
+			if (*str && ft_strchr("cspdiuxX%", (int)*str))
+				str++;
+		}
+		else
+			len += write(1, str++, 1);
+	}
+	va_end(args);
+	return (len);
+}
